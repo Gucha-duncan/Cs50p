d7dbd33e72d3fc5dcf44fb3b1ba6e423101a4729
Gucha_duncan <gucha_duncan@outlook.com>
2025-10-08 08:46:48 +0300

    List of dictionaries with for loops

--- loops/dictionaries.py
+++ loops/dictionaries.py
@@ -7,3 +7,16 @@ students = {
 
 for student in students:
     print(f"{student} lives in {students[student]}")
+    
+    
+cities = [
+    {'name': "Nairobi", 'country':'Kenya', 'continent': 'Africa'},
+     {'name': "London", 'country':'England', 'continent': 'Europe'},
+       {'name': "New York", 'country':'USA', 'continent': 'North America'},
+         {'name': "Kampala", 'country':'Uganda', 'continent': 'Africa'},
+           {'name': "Kisumu", 'country':'Kenya', 'continent': 'Africa'},
+]
+
+for city in cities:
+    print(f"{city['name']} is located in {city['country']}, {city['continent']}")
+ 
