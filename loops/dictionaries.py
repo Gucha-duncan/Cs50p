d1dc2b2b692301ce57b7e6a19843817cd50bd545
Gucha_duncan <gucha_duncan@outlook.com>
2025-10-08 08:22:56 +0300

    Loops on dictionaries

---
+++ loops/dictionaries.py
@@ -0,0 +1,9 @@
+students = {
+    'Javy': 'Eldoret',
+    'ManuBett': 'Kericho',
+    'Alphonce': 'Siaya',
+    'Mulu':'Ukambani',
+}
+
+for student in students:
+    print(f"{student} lives in {students[student]}")
---
+++ loops/students.py
@@ -0,0 +1,5 @@
+students = ["Jack", "Chacha", "Vivian", "Clinton"]
+
+
+for student in students:
+    print(f"Good morning {student}")
